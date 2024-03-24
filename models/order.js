b3a895ab600b2ad24e4f347a43407b14955ddfd6
Feraz Khan <ferazahmadgit62@gmail.com>
2024-03-24 11:33:37 +0530

    order id must be equal to cart id

--- models/order.js
+++ models/order.js
@@ -4,7 +4,6 @@ const sequelize = require("../utils/database.js");
 const Order = sequelize.define("order", {
   id: {
     type: DataTypes.INTEGER,
-    autoIncrement: true,
     allowNull: false,
     unique: true,
     primaryKey: true,
